07031421f1ecdc0b817bcca2a3293a31e0ba6707
Neto Deolino <netodeolino@gmail.com>
2021-03-06 16:34:20 -0300

    better solution for isUnique question

--- cracking-the-coding-interview/arrays-and-strings/IsUnique.java
+++ cracking-the-coding-interview/arrays-and-strings/IsUnique.java
@@ -1,7 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 // Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
 class IsUnique {
 
+	// slow
 	public static Boolean isUnique(String A) {
 		for (int i = 0; i < A.length(); i++) {
 			for (int j = i+1; j < A.length(); j++) {
@@ -13,12 +17,27 @@ class IsUnique {
 		return true;
 	}
 
+	// fast
+	public static Boolean isUnique2(String A) {
+		Map<Character, Integer> hashMap = new HashMap<>();
+
+		for (int i = 0; i < A.length(); i++) {
+			if (hashMap.containsKey(A.charAt(i))) {
+				return false;
+			} else {
+				hashMap.put(A.charAt(i), 1);
+			}
+		}
+
+		return true;
+	}
+
 	public static void main(String[] args) {
 		String testTrue = "abcdefghijlmno";
 		String testFalse = "abcdefghijlmnod";
 
-		System.out.println(isUnique(testTrue));
-		System.out.println(isUnique(testFalse));
+		System.out.println(isUnique2(testTrue));
+		System.out.println(isUnique2(testFalse));
 	}
 
 }
